3e69c3cbd358f8ebf515b29559c9baab33b5bccc
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-04 14:23:57 +0330

    initial commit

---
+++ src/main/java/com/dotin/CustomerInformationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dotin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInformationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInformationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotin/CustomerInformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInformationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
