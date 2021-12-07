9c5774bc18b0b9f08b72443b81e18fd53d775932
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-07 18:24:36 +0330

    customer type property added

--- src/main/java/com/dotin/model/data/CustomerType.java
+++ src/main/java/com/dotin/model/data/CustomerType.java
@@ -1,8 +1,17 @@
 package com.dotin.model.data;
 
+import lombok.Getter;
+
 /**
  * @author : Bahar Zolfaghari
  **/
 public enum CustomerType {
-    INDIVIDUAL, LEGAL
+    INDIVIDUAL("حقیقی"), LEGAL("حقوقی");
+
+    @Getter
+    private final String customerType;
+
+    CustomerType(String customerType) {
+        this.customerType = customerType;
+    }
 }
