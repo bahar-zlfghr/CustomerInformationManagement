e7511848f2130d58771e30a21fa4b42d3ad158fd
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-07 18:28:13 +0330

    class name changed

--- src/main/java/com/dotin/model/repository/IndividualCustomerRepository.java
+++ src/main/java/com/dotin/model/repository/IndividualCustomerRepository.java
@@ -1,13 +1,17 @@
 package com.dotin.model.repository;
 
 import com.dotin.model.data.Customer;
+import com.dotin.model.data.IndividualCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * @author : Bahar Zolfaghari
  **/
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface IndividualCustomerRepository extends JpaRepository<IndividualCustomer, Integer> {
 
+    Optional<Customer> findByNationalCode(String nationalCode);
 }
