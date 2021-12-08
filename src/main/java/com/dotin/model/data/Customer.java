11cf4c37d0071654e855cd8f2dd1583a48cd7804
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-08 14:33:10 +0330

    table name change and customer number set as table primary key and custom sequence generator defined on it

--- src/main/java/com/dotin/model/data/Customer.java
+++ src/main/java/com/dotin/model/data/Customer.java
@@ -2,6 +2,8 @@ package com.dotin.model.data;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.*;
 
@@ -9,17 +11,24 @@ import javax.persistence.*;
  * @author : Bahar Zolfaghari
  **/
 @Entity(name = "Customer")
+@Table(name = "Customer")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    private Integer customerID;
-
     @Getter
     @Setter
-    private String customerNO;
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "CustomerNumberSequence"),
+                    @Parameter(name = "initial_value", value = "10000001"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private Integer customerNO;
 
     @Getter
     @Setter
