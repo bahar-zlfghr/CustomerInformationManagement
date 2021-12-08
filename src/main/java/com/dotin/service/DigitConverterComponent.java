4b4d23caf701491711c9cd6e6526cb94e3a13383
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-08 19:14:33 +0330

    digit converter component added

---
+++ src/main/java/com/dotin/service/DigitConverterComponent.java
@@ -0,0 +1,29 @@
+package com.dotin.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+@Component
+public interface DigitConverterComponent {
+
+    static String convertDigitsEnToFa(String text) {
+        Character[] persianDigits = {'۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'};
+        List<Character> persianChars = Arrays.asList(persianDigits);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char currentCharacter = text.charAt(i);
+            if (!persianChars.contains(currentCharacter) && Character.isDigit(currentCharacter)) {
+                stringBuilder.append(persianChars.get((int) currentCharacter - 48));
+            }
+            else {
+                stringBuilder.append(currentCharacter);
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
