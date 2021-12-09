654364ad67815496827a8f1c265dab2517cc2bbf
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-09 16:05:36 +0330

    fina all and delete individual customer methods added

--- src/main/java/com/dotin/service/individualcustomer/IndividualCustomerService.java
+++ src/main/java/com/dotin/service/individualcustomer/IndividualCustomerService.java
@@ -3,10 +3,14 @@ package com.dotin.service.individualcustomer;
 import com.dotin.exception.DuplicateIndividualCustomerException;
 import com.dotin.model.data.IndividualCustomer;
 
+import java.util.List;
+
 /**
  * @author : Bahar Zolfaghari
  **/
 public interface IndividualCustomerService {
 
     IndividualCustomer saveIndividualCustomer(IndividualCustomer individualCustomer) throws DuplicateIndividualCustomerException;
+    List<IndividualCustomer> findAllIndividualCustomers();
+    void deleteIndividualCustomer(Integer customerNO);
 }
