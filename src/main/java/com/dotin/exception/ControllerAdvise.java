9cc710497697f7da6aefc4dae831e15c9fafbb73
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-09 10:10:35 +0330

    duplicate legal customer exception handling method added

--- src/main/java/com/dotin/exception/ControllerAdvise.java
+++ src/main/java/com/dotin/exception/ControllerAdvise.java
@@ -3,6 +3,7 @@ package com.dotin.exception;
 import com.dotin.model.CustomerFactory;
 import com.dotin.model.data.CustomerType;
 import com.dotin.model.data.IndividualCustomer;
+import com.dotin.model.data.LegalCustomer;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +21,16 @@ public class ControllerAdvise {
         modelAndView.addObject("duplicateIndividualCustomerException", e.getMessage());
         modelAndView.addObject("individualCustomer", individualCustomer);
         modelAndView.setViewName("individual-customer-registration");
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = DuplicateLegalCustomerException.class)
+    public ModelAndView duplicateLegalException(DuplicateLegalCustomerException e) {
+        ModelAndView modelAndView = new ModelAndView();
+        LegalCustomer legalCustomer = (LegalCustomer) CustomerFactory.createCustomer(CustomerType.LEGAL);
+        modelAndView.addObject("duplicateLegalCustomerException", e.getMessage());
+        modelAndView.addObject("legalCustomer", legalCustomer);
+        modelAndView.setViewName("legal-customer-registration");
         return modelAndView;
     }
 }
