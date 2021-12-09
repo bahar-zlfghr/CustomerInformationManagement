9060f34b8ce717236dd802b2049bd6bdaa01f014
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-09 16:04:36 +0330

    view customers list page method added

--- src/main/java/com/dotin/controller/MainController.java
+++ src/main/java/com/dotin/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.dotin.controller;
 
+import com.dotin.service.individualcustomer.IndividualCustomerService;
+import com.dotin.service.legalcustomer.LegalCustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 @Controller
 public class MainController {
+    private final IndividualCustomerService individualCustomerService;
+    private final LegalCustomerService legalCustomerService;
 
-    @GetMapping(value = "/")
-    public String viewIndexPage() {
-        return "index";
+    public MainController(IndividualCustomerService individualCustomerService, LegalCustomerService legalCustomerService) {
+        this.individualCustomerService = individualCustomerService;
+        this.legalCustomerService = legalCustomerService;
+    }
+
+    @GetMapping(value = "/customers")
+    public String viewCustomersListPage(Model model) {
+        model.addAttribute("individualCustomers", individualCustomerService.findAllIndividualCustomers());
+        model.addAttribute("legalCustomers", legalCustomerService.findAllLegalCustomers());
+        return "customers-list";
     }
 }
