6924a0b1a14629f287e1e2bf58b0c46a2c7248f6
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-11 18:07:36 +0330

    two methods added for handling real and legal customer not found exception

--- src/main/java/com/dotin/exception/ControllerAdvise.java
+++ src/main/java/com/dotin/exception/ControllerAdvise.java
@@ -1,12 +1,11 @@
 package com.dotin.exception;
 
-import com.dotin.model.CustomerFactory;
-import com.dotin.model.data.CustomerType;
-import com.dotin.model.data.IndividualCustomer;
-import com.dotin.model.data.LegalCustomer;
+import com.dotin.util.CustomerDtoUtil;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author : Bahar Zolfaghari
@@ -14,23 +13,33 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class ControllerAdvise {
 
-    @ExceptionHandler(value = DuplicateIndividualCustomerException.class)
-    public ModelAndView duplicateIndividualCustomerException(DuplicateIndividualCustomerException e) {
+    @ExceptionHandler(value = DuplicateRealCustomerException.class)
+    public ModelAndView duplicateIndividualCustomerException(DuplicateRealCustomerException e) {
         ModelAndView modelAndView = new ModelAndView();
-        IndividualCustomer individualCustomer = (IndividualCustomer) CustomerFactory.createCustomer(CustomerType.INDIVIDUAL);
-        modelAndView.addObject("duplicateIndividualCustomerException", e.getMessage());
-        modelAndView.addObject("individualCustomer", individualCustomer);
-        modelAndView.setViewName("individual-customer-registration");
+        modelAndView.addObject("duplicateRealCustomerException", e.getMessage());
+        modelAndView.addObject("realCustomer", CustomerDtoUtil.createRealCustomer());
+        modelAndView.setViewName("real-customer-registration");
         return modelAndView;
     }
 
     @ExceptionHandler(value = DuplicateLegalCustomerException.class)
     public ModelAndView duplicateLegalCustomerException(DuplicateLegalCustomerException e) {
         ModelAndView modelAndView = new ModelAndView();
-        LegalCustomer legalCustomer = (LegalCustomer) CustomerFactory.createCustomer(CustomerType.LEGAL);
         modelAndView.addObject("duplicateLegalCustomerException", e.getMessage());
-        modelAndView.addObject("legalCustomer", legalCustomer);
+        modelAndView.addObject("legalCustomer", CustomerDtoUtil.createLegalCustomer());
         modelAndView.setViewName("legal-customer-registration");
         return modelAndView;
+    }
+
+    @ExceptionHandler(value = RealCustomerNotFoundException.class)
+    public String realCustomerNotFoundException(RealCustomerNotFoundException e, HttpSession httpSession) {
+        httpSession.setAttribute("realCustomerNotFoundException", e.getMessage());
+        return "redirect:http://localhost:8080/customers";
+    }
+
+    @ExceptionHandler(value = LegalCustomerNotFoundException.class)
+    public String legalCustomerNotFoundException(LegalCustomerNotFoundException e, HttpSession httpSession) {
+        httpSession.setAttribute("legalCustomerNotFoundException", e.getMessage());
+        return "redirect:http://localhost:8080/customers";
     }
 }
