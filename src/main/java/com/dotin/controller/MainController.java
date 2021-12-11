7abb9fbea7c2c8bc859a41570df9695487dc5aa0
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-11 18:14:30 +0330

    minor changes

--- src/main/java/com/dotin/controller/MainController.java
+++ src/main/java/com/dotin/controller/MainController.java
@@ -1,6 +1,6 @@
 package com.dotin.controller;
 
-import com.dotin.service.individualcustomer.IndividualCustomerService;
+import com.dotin.service.realcustomer.RealCustomerService;
 import com.dotin.service.legalcustomer.LegalCustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 @Controller
 public class MainController {
-    private final IndividualCustomerService individualCustomerService;
+    private final RealCustomerService realCustomerService;
     private final LegalCustomerService legalCustomerService;
 
-    public MainController(IndividualCustomerService individualCustomerService, LegalCustomerService legalCustomerService) {
-        this.individualCustomerService = individualCustomerService;
+    public MainController(RealCustomerService realCustomerService, LegalCustomerService legalCustomerService) {
+        this.realCustomerService = realCustomerService;
         this.legalCustomerService = legalCustomerService;
     }
 
     @GetMapping(value = "/customers")
     public String viewCustomersListPage(Model model) {
-        model.addAttribute("individualCustomers", individualCustomerService.findAllIndividualCustomers());
+        model.addAttribute("realCustomers", realCustomerService.findAllRealCustomers());
         model.addAttribute("legalCustomers", legalCustomerService.findAllLegalCustomers());
         return "customers-list";
     }
