a400cdf38475ac3898597b496a2c24f07bda7346
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-11 18:08:32 +0330

    convert farsi to english digits method added

--- src/main/java/com/dotin/service/component/DigitConverterComponent.java
+++ src/main/java/com/dotin/service/component/DigitConverterComponent.java
@@ -9,10 +9,11 @@ import java.util.List;
  * @author : Bahar Zolfaghari
  **/
 @Component
-public interface DigitConverterComponent {
+public class DigitConverterComponent {
+    private static final Character[] persianDigits = {'۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'};
+    private static final Character[] englishDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-    static String convertDigitsEnToFa(String text) {
-        Character[] persianDigits = {'۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'};
+    public static String convertEnToFa(String text) {
         List<Character> persianChars = Arrays.asList(persianDigits);
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
@@ -25,5 +26,21 @@ public interface DigitConverterComponent {
             }
         }
         return stringBuilder.toString();
+    }
+
+    public static Integer convertFaToEn(String text) {
+        List<Character> persianChars = Arrays.asList(persianDigits);
+        List<Character> englishChars = Arrays.asList(englishDigits);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char currentCharacter = text.charAt(i);
+            if (persianChars.contains(currentCharacter)) {
+                stringBuilder.append(englishChars.get((int) currentCharacter - 1776));
+            }
+            else {
+                stringBuilder.append(currentCharacter);
+            }
+        }
+        return Integer.valueOf(stringBuilder.toString());
     }
 }
