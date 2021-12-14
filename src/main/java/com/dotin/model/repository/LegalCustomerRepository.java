aaf3b3e0e0c0c1b5a43d6007b566334ee40fb39e
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-14 12:17:21 +0330

    implemented jpa specification executor added

--- src/main/java/com/dotin/model/repository/LegalCustomerRepository.java
+++ src/main/java/com/dotin/model/repository/LegalCustomerRepository.java
@@ -2,6 +2,7 @@ package com.dotin.model.repository;
 
 import com.dotin.model.data.LegalCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
  * @author : Bahar Zolfaghari
  **/
 @Repository
-public interface LegalCustomerRepository extends JpaRepository<LegalCustomer, Integer> {
+public interface LegalCustomerRepository extends JpaRepository<LegalCustomer, Integer>, JpaSpecificationExecutor<LegalCustomer> {
 
     Optional<LegalCustomer> findByCode(String economicCode);
     Optional<LegalCustomer> findByCustomerNO(Integer customerNo);
