f90a5ffc0402c04296f01933f797ff83bda4e3a5
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-14 11:48:18 +0330

    real customer specification added

---
+++ src/main/java/com/dotin/model/repository/RealCustomerSpecification.java
@@ -0,0 +1,37 @@
+package com.dotin.model.repository;
+
+import com.dotin.model.data.RealCustomer;
+import com.dotin.service.component.DigitConverterComponent;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public interface RealCustomerSpecification {
+
+    static Specification<RealCustomer> search(String firstName, String lastName, String nationalCode, String customerNO) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            CriteriaQuery<RealCustomer> customerCriteriaQuery = criteriaBuilder.createQuery(RealCustomer.class);
+            if (!Objects.isNull(firstName) && !firstName.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("name"), firstName));
+            }
+            if (!Objects.isNull(lastName) && !lastName.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("lastName"), lastName));
+            }
+            if (!Objects.isNull(nationalCode) && !nationalCode.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("code"), nationalCode));
+            }
+            if (!Objects.isNull(customerNO) && !customerNO.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("customerNO"), DigitConverterComponent.convertFaToEn(customerNO)));
+            }
+            return customerCriteriaQuery.select(root).where(predicates.toArray(new Predicate[0])).getRestriction();
+        };
+    }
+}
