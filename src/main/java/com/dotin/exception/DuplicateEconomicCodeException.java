6e00dfb4615b53160415f50ea995281f72d7269d
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-15 09:45:06 +0330

    duplicate economic exception added

---
+++ src/main/java/com/dotin/exception/DuplicateEconomicCodeException.java
@@ -0,0 +1,11 @@
+package com.dotin.exception;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class DuplicateEconomicCodeException extends RuntimeException {
+
+    public DuplicateEconomicCodeException(String message) {
+        super(message);
+    }
+}
