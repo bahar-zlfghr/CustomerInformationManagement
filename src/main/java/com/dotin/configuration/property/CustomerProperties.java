3a228c934bbb9f00f74c1a5dd1108c46282f8e06
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-25 17:11:30 +0330

    customer properties added

---
+++ src/main/java/com/dotin/configuration/property/CustomerProperties.java
@@ -0,0 +1,13 @@
+package com.dotin.configuration.property;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+@Component
+public class CustomerProperties {
+
+    public static final String REAL = "0";
+    public static final String LEGAL = "1";
+}
