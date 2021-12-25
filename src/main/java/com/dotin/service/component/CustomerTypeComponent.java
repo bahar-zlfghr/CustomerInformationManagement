71a543f8e01c184ae4eb7f9172d4a4742dc8fee2
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-25 15:35:43 +0330

    dynamic switch case implemented by if-else

--- src/main/java/com/dotin/service/component/CustomerTypeComponent.java
+++ src/main/java/com/dotin/service/component/CustomerTypeComponent.java
@@ -7,22 +7,16 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class CustomerTypeComponent {
-    private final String REAL;
-    private final String LEGAL;
-
-    public CustomerTypeComponent(PropertyReaderComponent propertyReaderComponent) {
-        this.REAL = propertyReaderComponent.getProperty("customer.type.real");
-        this.LEGAL = propertyReaderComponent.getProperty("customer.type.legal");
-    }
 
     public String getCustomerType(String key) {
-        switch (key) {
-            case "0":
-                return REAL;
-            case "1":
-                return LEGAL;
-            default:
-                return null;
+        if (key.equals(PropertyReaderComponent.getProperty("customer.type.real.binary"))) {
+            return PropertyReaderComponent.getProperty("customer.type.real.text");
+        }
+        else if (key.equals(PropertyReaderComponent.getProperty("customer.type.legal.binary"))) {
+            return PropertyReaderComponent.getProperty("customer.type.legal.text");
+        }
+        else {
+            return null;
         }
     }
 }
