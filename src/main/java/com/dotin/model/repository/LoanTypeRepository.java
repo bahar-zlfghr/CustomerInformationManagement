fd6cef792aa5433b729bfe79f22026e996cc0f35
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-12-30 18:48:28 +0330

    useless method removed

--- src/main/java/com/dotin/model/repository/LoanTypeRepository.java
+++ src/main/java/com/dotin/model/repository/LoanTypeRepository.java
@@ -4,13 +4,10 @@ import com.dotin.model.data.LoanType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 /**
  * @author : Bahar Zolfaghari
  **/
 @Repository
 public interface LoanTypeRepository extends JpaRepository<LoanType, Integer> {
 
-    Optional<LoanType> findById(Integer ID);
 }
